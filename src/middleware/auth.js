0126d641945e5dd8a78db38415b194e7f9ba0d08
Ankur Kundu <ankur@codelogicx.com>
2021-12-21 13:08:21 +0530

    Auth middleware secret check fix

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/user')
 const auth = async (req, res, next) => {
     try{
         const token = req.header('Authorization').replace('Bearer ', '')
-        const decoded = jwt.verify(token, 'Yg7gbuIYGAog98wK56erhajbfy8tgkDFwUYRE2g31fb5MJGr2875t8bKgfMMws')
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
         if(!user){
